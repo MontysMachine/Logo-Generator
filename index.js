36ce2e288d15c54a518b0c71d220ddf53e4507e4
Monty Montgomery <monty@montys-mbp.attlocal.net>
2024-03-07 14:12:37 -0600

    solved previous problem by instead of creating an svg file i created an html file and add my logo html to it. new problem is the text is not aligned to the center of the shape

--- index.js
+++ index.js
@@ -2,57 +2,65 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const {Logo, Square, Triangle, Circle } = require('./lib/shapes.js');
 
+
 inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'Please enter 3 characters to be used in your logo\n',
-      name: 'text'
-    },
-    {
-      type: 'input',
-      message: 'Please enter the color keyword or hexadecimal number for your logo\n',
-      name: 'color'
-    },
-    {
-      type: 'list',
-      message: 'Please choose a shape for your logo',
-      choices: ['Square', 'Triangle', 'Circle'],
-      name: 'shape'
+.prompt([
+{
+    type: 'input',
+    message: 'Please enter 3 characters to be used in your logo\n',
+    name: 'text'
+},
+{
+    type: 'input',
+    message: 'Please enter the color keyword or hexadecimal number for your logo\n',
+    name: 'color'
+},
+{
+    type: 'list',
+    message: 'Please choose a shape for your logo',
+    choices: ['Square', 'Triangle', 'Circle'],
+    name: 'shape'
+}
+])
+.then(answers => {
+const shape = answers.shape;
+const text = answers.text;
+const color = answers.color;
+
+let logo;
+switch (shape) {
+    case 'Square':
+    logo = new Square(text, color);
+    break;
+    case 'Triangle':
+    logo = new Triangle(text, color);
+    break;
+    case 'Circle':
+    logo = new Circle(text, color);
+    break;
+    default:
+    console.log('Invalid answers');
+    return;
+}
+
+
+const htmlContent = `<html>
+    <head>
+    <title>Logos</title>
+    </head>
+    <body>
+        ${logo.generateSVG()}
+    </body>
+    </html>`;
+
+fs.writeFile('./examples/logos.html', htmlContent, err => {
+    if (err) {
+    console.error('Error writing HTML file:', err);
+    } else {
+    console.log('HTML file saved successfully: ./examples/logos.html');
     }
-  ])
-  .then(answers => {
-    const shape = answers.shape;
-    const text = answers.text;
-    const color = answers.color;
-
-    let logo;
-    switch (shape) {
-      case 'Square':
-        logo = new Square(text, color);
-        break;
-      case 'Triangle':
-        logo = new Triangle(text, color);
-        break;
-      case 'Circle':
-        logo = new Circle(text, color);
-        break;
-      default:
-        console.log('Invalid answers');
-        return;
-    }
-
-    const finishedLogo = logo.generateSVG();
-
-    fs.writeFile('./examples/logo.svg', finishedLogo, (err) => {
-      if (err) {
-        console.log('Error generating your logo:', err);
-        return;
-      } else {
-        console.log('Your logo has been generated!');
-      }
-    });
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+});
+})
+.catch(error => {
+console.error('Error:', error);
+});
